d0f0b011c499ee1352d5e4aa51c90cddba20d165
Abhishek Shroff <ashroff6@gmail.com>
2021-03-26 00:14:55 +0530

    Modularize code, MdiIcons.fromString

--- index.js
+++ index.js
@@ -1,55 +1,20 @@
 const util = require("@mdi/util");
 
-const meta = util.getMeta(true);
-
-const find = /(\-\w)/g;
-const convert = function (matches) {
-  return matches[1].toUpperCase();
-};
-
-const lines = meta.map((icon) => {
-  let name = icon.name.replace(find, convert);
-  name = `${name[0].toLowerCase()}${name.slice(1)}`;
-
-  if (["null", "switch", "sync", "factory"].includes(name)) {
-    name = `${name}Icon`;
-  }
-
-  return `  /// Icon for ${icon.name}.
-  static const ${name} = MdiIconData(0x${icon.codepoint});
-`;
-});
-
-const output = `// Material Design Icons v${util.getVersion()}
-
-library mdi;
-
-import 'package:flutter/widgets.dart';
-
-const _fontFamily = 'Material Design Icons';
-const _packageName = 'mdi';
-
-/// A const wrapper for [IconData].
-class MdiIconData extends IconData {
-  const MdiIconData(int codePoint)
-      : super(codePoint, fontFamily: _fontFamily, fontPackage: _packageName);
-}
-
-/// Mdi is a collection of icons provided by
-/// [Material Design Icons](https://materialdesignicons.com/).
-class Mdi {
-${lines.join("\n")}
-}
-`;
-util.write("mdi/lib/mdi.dart", output);
+const meta = util.getMeta();
 
 processTemplate('mdi.dart', {
-  'VERSION': util.getVersion()
+  'VERSION': util.getVersion(),
+});
+
+processTemplate('icon_map.dart', {
+  'ICON_MAP': meta.map((icon) => {
+    return `  '${processName(icon.name)}': 0x${icon.codepoint},`;
+  }).join('\n'),
 });
 
 
 function processTemplate(path, params) {
-  const template = util.read('templates/' + path);
+  const template = util.read('templates/' + path + '.template');
   const output = template.split('\n').map((line) => {
     return line.replace(/<%\w+%>/g, (param) => {
       const name = param.replace(/\W/g, '');
@@ -58,4 +23,16 @@ function processTemplate(path, params) {
   }).join('\n');
 
   util.write('mdi/lib/' + path, output);
+}
+
+function processName(name) {
+  name = name.replace(/(\-\w)/g, (matches) => {
+    return matches[1].toUpperCase();
+  });
+  name = `${name[0].toLowerCase()}${name.slice(1)}`;
+
+  if (["null", "switch", "sync", "factory"].includes(name)) {
+    name = `${name}Icon`;
+  }
+  return name;
 }
